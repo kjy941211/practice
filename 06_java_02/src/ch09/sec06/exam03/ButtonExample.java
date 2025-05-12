35a789ac088a6d0c218a7ed22de6ffb9dd26c535
kjy941211 <kjy941211@gmail.com>
2025-05-12 22:34:45 +0900

    practice

---
+++ 06_java_02/src/ch09/sec06/exam03/ButtonExample.java
@@ -0,0 +1,19 @@
+package ch09.sec06.exam03;
+
+import java.awt.event.ActionListener;
+
+public class ButtonExample {
+    public static void main(String[] args) {
+//Ok 버튼 객체 생성
+        Button btnOk = new Button();
+        class OkListener implements Button.ClickListener {
+            @Override
+            public void onClick() {
+                System.out.println("OK 버튼을 클릭했습니다.");
+            }
+        }
+        btnOk.setClickListener(new OkListener());
+//Ok 버튼 클릭하기
+        btnOk.click();
+    }
+}
