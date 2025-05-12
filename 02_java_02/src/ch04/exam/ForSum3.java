25d7f28f1866d4724e68ca6773e29469246de13c
kjy941211 <kjy941211@gmail.com>
2025-05-12 22:33:22 +0900

    practice

---
+++ 02_java_02/src/ch04/exam/ForSum3.java
@@ -0,0 +1,15 @@
+package ch04.exam;
+
+public class ForSum3 {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
