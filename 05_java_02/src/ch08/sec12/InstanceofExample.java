e22cf59759315852ef36a5ee7617a2391d135bc1
kjy941211 <kjy941211@gmail.com>
2025-05-12 22:34:25 +0900

    practice

---
+++ 05_java_02/src/ch08/sec12/InstanceofExample.java
@@ -0,0 +1,25 @@
+package ch08.sec12;
+
+public class InstanceofExample {
+    public static void main(String[] args) {
+
+        Taxi taxi = new Taxi();
+        Bus bus = new Bus();
+
+        ride(taxi);
+        ride(bus);
+    }
+    //인터페이스 매개변수를 갖는 메소드
+    public static void ride(Vehicle vehicle) {
+//방법1
+/*if(vehicle instanceof Bus) {
+Bus bus = (Bus) vehicle;
+bus.checkFare();
+}*/
+//방법2
+        if (vehicle instanceof Bus bus) {
+            bus.checkFare();
+        }
+        vehicle.run();
+    }
+}
